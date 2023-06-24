a814a5ee1748f4489ded92e88e3e19bcb5cf089a
GoylikDmitriy <goylik.dmitriy@gmail.com>
2023-06-24 19:30:20 +0300

    initial

---
+++ src/main/java/com/goylik/questionsPortal/questionsPortal/QuestionsPortalApplication.java
@@ -0,0 +1,15 @@
+package com.goylik.questionsPortal.questionsPortal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan("com.goylik.questionsPortal")
+public class QuestionsPortalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuestionsPortalApplication.class, args);
+	}
+
+}
